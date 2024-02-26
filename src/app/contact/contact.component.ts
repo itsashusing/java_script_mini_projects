3de5d7741168693dbeffe62c00cd10321708eb23
Ashutosh Singh <itsmemyself710@gmail.com>
2024-02-26 20:02:25 +0530

    portfolio

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css',
+})
+export class ContactComponent {
+  name!: string;
+  submit() {
+    if (this.name !== null && this.name !== undefined) {
+      alert(`Sir ${this.name}I will contact you soon.`);
+      window.location.href = '';
+    } else {
+      console.log(this.name);
+      alert('All field are mandatory.');
+    }
+  }
+}
